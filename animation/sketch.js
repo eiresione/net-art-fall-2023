558d7ffbced7f94bd080c06e06d14858f242702b
Natalia Czepielewski <144501349+eiresione@users.noreply.github.com>
2023-11-04 22:30:07 -0400

    Add files via upload

---
+++ animation/sketch.js
@@ -0,0 +1,21 @@
+var y = 0
+function setup(){
+  createCanvas(windowWidth, windowHeight);
+   stroke(16, 84, 230);
+  fill(103, 178, 224);
+  ellipseMode(CENTER);
+ 
+}
+
+function draw(){
+  background(255);
+  translate(windowWidth/2, windowHeight/2);
+
+  rotate(millis()/1000);
+  ellipse(y, y, 300, 300, 30);
+  drawingContext.filter = 'blur(150px)';
+  
+  y = mouseX
+
+
+}
